29edc5f213e867217ed15e412a709e587a060138
Alexandru Bese <alexandru.bese@3pillarglobal.com>
2022-05-05 17:16:09 +0300

    Some more styling

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -19,18 +19,18 @@ const CountryDetails = (props) => {
                     <div className={classes.info}>
                         <div>
                             <p>Native name: {props.countryDetails.nativeName}</p>
-                            <p>Population: {props.countryDetails.population}</p>
+                            <p>Population: {props.countryDetails.population.toLocaleString()}</p>
                             <p>Region: {props.countryDetails.region}</p>
                             <p>Sub Region: {props.countryDetails.subregion}</p>
                             <p>Capital: {props.countryDetails.capital}</p>
                         </div>
 
                         <div className={classes.column}>
-                            <p>Top LevelDomain: {props.countryDetails.topLevelDomain && props.countryDetails.topLevelDomain.map(tld => <span>{tld}</span>)}</p>
-                            <p>Currencies: {props.countryDetails.currencies && props.countryDetails.currencies.map(currency => <span>{currency.name}</span>)}</p>
-                            <p>Langugaes: {props.countryDetails.languages && props.countryDetails.languages.map(language => <span>{language.name}</span>)}</p>
+                            <p>Top LevelDomain: {props.countryDetails.topLevelDomain && props.countryDetails.topLevelDomain.map(tld => <span>{` ${tld} |`}</span>)}</p>
+                            <p>Currencies: {props.countryDetails.currencies && props.countryDetails.currencies.map(currency => <span>{` ${currency.name} |`}</span>)}</p>
+                            <p>Languages: {props.countryDetails.languages && props.countryDetails.languages.map(language => <span>{` ${language.name} |`}</span>)}</p>
 
-                            <p>Border Countries: {props.countryDetails.borders && props.countryDetails.borders.map(border => <span>{border}</span>)}</p>
+                            <p>Border Countries: {props.countryDetails.borders && props.countryDetails.borders.map(border => <span>{` ${border} |`}</span>)}</p>
                         </div>
                     </div>
                 </div>
